f6ff66d6d44d96f5966ae804229598bd8ad32eeb
Andrew Wyatt <a.wyatt@stratec.com>
2019-05-13 14:48:30 +0100

    Global DI of service.  Need to mock some of them out.

--- ng-fundamentals/src/app/events/events-list.component.ts
+++ ng-fundamentals/src/app/events/events-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { EventService } from './shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'events-list',
@@ -9,7 +10,7 @@ import { EventService } from './shared/event.service';
     <hr />
     <div class="row">
       <div *ngFor="let event of events" class="col-md-5">
-        <event-thumbnail [event]="event"></event-thumbnail>
+        <event-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></event-thumbnail>
       </div>
     </div>
   </div>
@@ -18,7 +19,9 @@ import { EventService } from './shared/event.service';
 export class EventsListComponent implements OnInit {
   events: any[];
 
-  constructor(private eventService: EventService) {
+  constructor(
+    private eventService: EventService,
+    private toastrService: ToastrService) {
   }
 
   ngOnInit() {
@@ -27,5 +30,9 @@ export class EventsListComponent implements OnInit {
 
   handleEventClicked(event) {
     console.log(event);
+  }
+
+  handleThumbnailClick(eventName) {
+    this.toastrService.success(eventName);
   }
 }
