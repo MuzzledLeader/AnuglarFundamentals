bd72063b4adea2de52bd7c6306e42e3fa82dfdab
Andrew Wyatt <a.wyatt@stratec.com>
2019-05-13 14:36:04 +0100

    DI

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { EventsAppComponent } from './events-app.component';
 import { EventsListComponent } from './events/events-list.component';
 import { EventThumbnailComponent } from ".\\events\\event-thumbnail.component";
 import { NavBarComponent } from './nav/navbar.components';
+import { EventService } from './events/shared/event.service';
 
 @NgModule({
   imports: [
     BrowserModule
   ],
-    declarations: [
+  declarations: [
       EventsAppComponent,
       EventsListComponent,
       EventThumbnailComponent,
       NavBarComponent
+  ],
+  providers: [
+      EventService
   ],
   bootstrap: [EventsAppComponent]
 })
