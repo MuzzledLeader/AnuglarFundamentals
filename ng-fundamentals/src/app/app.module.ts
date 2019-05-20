756c61e398907017597d894a7a0abd13871c88b3
Andrew Wyatt <a.wyatt@stratec.com>
2019-05-20 15:48:31 +0100

    import barals (index.ts) to make it easier to import components.
    routes.ts has a new child route. user/profile (with it's own routes)

--- ng-fundamentals/src/app/app.module.ts
+++ ng-fundamentals/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {
+  EventThumbnailComponent,
+  CreateEventComponent,
+  EventDetailsComponent,
+  EventService,
+  EventsListComponent
+} from "./events/index"
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from ".\\events\\event-thumbnail.component";
 import { NavBarComponent } from './nav/navbar.components';
-import { EventService } from './events/shared/event.service';
 import { ToastrService } from './common/toastr.service';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from '../routes';
-import { CreateEventComponent } from "./events/create-event.component"
 
 @NgModule({
   imports: [
--- ng-fundamentals/src/routes.ts
+++ ng-fundamentals/src/routes.ts
@@ -1,12 +1,15 @@
 import { Routes } from '@angular/router';
 
-import { EventsListComponent } from "./app/events/events-list.component";
-import { EventDetailsComponent } from "./app/events/event-details/event-details.component";
-import { CreateEventComponent } from './app/events/create-event.component';
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent
+} from "./app/events/index"
 
 export const appRoutes:Routes = [
   { path: 'events', component: EventsListComponent },
   { path: 'events/new', component: CreateEventComponent },
   { path: 'events/:id', component: EventDetailsComponent },
-  { path: '', redirectTo: '/events', pathMatch: 'full' }
+  { path: '', redirectTo: '/events', pathMatch: 'full' },
+  { path: 'user', loadChildren: './app/user/user.module#UserModule' }
 ];
