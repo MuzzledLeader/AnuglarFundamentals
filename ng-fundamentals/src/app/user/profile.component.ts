4b09dbee5ec78e0eabca9f0de500ccb59995f354
Andrew Wyatt <a.wyatt@stratec.com>
2019-05-22 15:00:03 +0100

    Reactive forms with custom errors.
    More control.
    
    ADDED - Validation methodology

--- ng-fundamentals/src/app/user/profile.component.ts
+++ ng-fundamentals/src/app/user/profile.component.ts
@@ -1,13 +1,23 @@
 import { Component, OnInit } from '@angular/core'
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from "./auth.service";
 import { Router } from '@angular/router';
 
 @Component({
-  templateUrl: './profile.component.html'
+  templateUrl: './profile.component.html',
+  styles: [`
+    em {float:right; color:#E05C65; padding-left: 10px;}
+    .error input {background-color:#E3C3C5;}
+    .error ::-webkit-input-placeholder {color: #999;}
+    .error ::-mox-placeholder {color: #999;}
+    .error :-moz-placeholder {color: #999;}
+    .error :m-input-placeholder {color: #999;}
+`]
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  firstname: FormControl;
+  lastname: FormControl;
 
   constructor(
     private authService: AuthService,
@@ -15,11 +25,15 @@ export class ProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    let firstname = new FormControl(this.authService.currentUser.firstname);
-    let lastname = new FormControl(this.authService.currentUser.lastname);
+    this.firstname = new FormControl(
+      this.authService.currentUser.firstname,
+      Validators.required);
+    this.lastname = new FormControl(
+      this.authService.currentUser.lastname,
+      Validators.required);
     this.profileForm = new FormGroup({
-      firstname: firstname,
-      lastname: lastname
+      firstname: this.firstname,
+      lastname: this.lastname
     });
   }
 
@@ -30,5 +44,13 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues) {
     this.authService.updateCurrentUser(formValues.firstname, formValues.lastname);
     this.router.navigate(['events']);
+  }
+
+  validateFirstname() {
+    return this.firstname.valid || this.firstname.untouched;
+  }
+
+  validateLastname() {
+    return this.lastname.valid || this.lastname.untouched;
   }
 }
