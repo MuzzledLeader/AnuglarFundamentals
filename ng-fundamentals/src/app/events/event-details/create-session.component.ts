490f795c6a130f6c6993c625e8a42388e6ace47f
Andrew Wyatt <a.wyatt@stratec.com>
2019-06-06 12:17:15 +0100

    OUTPUT from components which equiates to using EventEmitter and invoking an event.

--- ng-fundamentals/src/app/events/event-details/create-session.component.ts
+++ ng-fundamentals/src/app/events/event-details/create-session.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, Output, EventEmitter } from '@angular/core'
 import { FormControl, FormGroup, Validators } from '@angular/forms'
 import { ISession, restrictedWords } from "../shared/index";
 
 @Component({
   templateUrl: './create-session.component.html',
+  selector: "create-session",
   styles: [`
     em {float:right; color:#E05C65; padding-left: 10px;}
     .error input {background-color:#E3C3C5;}
@@ -15,6 +16,9 @@ import { ISession, restrictedWords } from "../shared/index";
 })
 
 export class CreateSessionComponent implements OnInit {
+  @Output() saveNewSession = new EventEmitter();
+  @Output() cancelAddSession = new EventEmitter();
+
   newSessionForm: FormGroup;
 
   name: FormControl;
@@ -56,6 +60,10 @@ export class CreateSessionComponent implements OnInit {
       voters: []
     }
 
-    console.log(session);
+    this.saveNewSession.emit(session);
+  }
+
+  cancel() {
+    this.cancelAddSession.emit();
   }
 }
